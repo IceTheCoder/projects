7e1dd7282d76070c936ddef9a0d9018b5a786de1
IceTheCoder <cristian.gabriel131408@gmail.com>
2024-05-26 09:24:30 +0300

    carousel pretty much works

--- photography-website/main.js
+++ photography-website/main.js
@@ -1,7 +1,30 @@
 const images = [
-  'img1.jpg',
-  'img2.jpg',
-  'img3.jpg'
+  'images/img1.jpg',
+  'images/img2.jpg',
+  'images/img3.jpg'
 ]
+let currentIndex = 0;
+const carouselImage = document.getElementById('carousel-img');
 
-console.log('Hello, world!');
+function showImage(index) {
+  // Ensure the index is within bounds
+  if (index < 0) {
+    currentIndex = images.length - 1;
+  } else if (index >= images.length) {
+    currentIndex = 0;
+  } else {
+    currentIndex = index;
+  }
+  carouselImage.src = images[currentIndex];
+}
+
+function showNextImage() {
+  showImage(currentIndex + 1);
+}
+
+function showPreviousImage() {
+  showImage(currentIndex - 1);
+}
+
+document.getElementById('next').addEventListener('click', showNextImage);
+document.getElementById('prev').addEventListener('click', showPreviousImage);
