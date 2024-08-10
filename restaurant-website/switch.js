3086c426ffd3282bd545e2bf7547d46e5e249477
IceTheCoder <cristian.gabriel131408@gmail.com>
2024-08-10 12:03:24 +0300

    light mode kept through home and menu tabs

--- restaurant-website/switch.js
+++ restaurant-website/switch.js
@@ -1,28 +1,37 @@
-// Dark/Light Mode Switch
-const button = document.getElementById("mode-switch");
-let mode = "dark";
-let buttons = document.getElementsByTagName("button");
+// https://www.shecodes.io/athena/41313-how-to-apply-a-function-to-a-specific-button-in-javascript
+// Wrapped in a function so it's only called once by index.html
+function mapButton() {
+  // Dark/Light Mode Switch
+  console.log("Mapping button");
+  const button = document.getElementById("mode-switch");
+  let mode = "dark";
+
+  button.addEventListener("click", function() {
+    if (mode === "dark") {
+      mode = "light";
+      lightMode();
+    } else if (mode === "light") {
+      mode = "dark";
+      darkMode();
+    }
+    // https://stackoverflow.com/questions/39838058/how-to-avoid-javascript-variable-being-reset-at-page-refresh
+    localStorage.setItem("mode", mode)
+    console.log(mode);
+  });  
+}
 
 // The elements with this class name are to be ignored when changing background colors
 let ignoredElementsClassName = "ignore";
-
-// https://www.shecodes.io/athena/41313-how-to-apply-a-function-to-a-specific-button-in-javascript
-button.addEventListener("click", function() {
-  if (mode === "dark") {
-    mode = "light";
-    lightMode();
-  } else if (mode === "light") {
-    mode = "dark";
-    darkMode();
-  }
-  console.log(mode);
-});
 
 /**
  * Update the page to light mode
  */
 function lightMode() {
-  button.innerHTML = "Switch to Dark Mode";
+  const button = document.getElementById("mode-switch");
+
+  if (button) {
+    button.innerHTML = "Switch to Dark Mode";
+  }
   document.body.style.backgroundColor = "rgb(255, 255, 255)";
 
   function setBackgroundColor(element, r, g, b) {
@@ -67,7 +76,10 @@ function lightMode() {
  * Update the page to dark mode
  */
 function darkMode() {
-  button.innerHTML = "Switch to Light Mode";
+  const button = document.getElementById("mode-switch");
+  if (button) {
+    button.innerHTML = "Switch to Light Mode";
+  }
   document.body.style.backgroundColor = "rgb(18, 18, 18)";
 
   function setBackgroundColor(element, r, g, b) {
@@ -106,4 +118,15 @@ function darkMode() {
   });
 
   setBackgroundColor(document.body, 34, 34, 34);
+}
+
+function checkMode() {
+  if (localStorage.getItem("mode") === "light") {
+    lightMode();  // Make sure this function is defined
+  }
+}
+
+function mainPageLoad() {
+  mapButton();
+  checkMode();
 }
