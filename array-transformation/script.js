b114b7e601d7f51936209799871bc3ad2e606fea
IceTheCoder <cristian.gabriel131408@gmail.com>
2024-09-18 18:42:11 +0300

    get text

---
+++ array-transformation/script.js
@@ -0,0 +1,15 @@
+let array = [];
+
+// Define function to parse array
+function parseArray() {
+
+// Define an empty array
+  array = [];
+// place the text of the input into a variable (string)
+  let text = document.getElementById("array").value;
+  console.log(text);
+// Read until a comma
+// push everything that has been read into the array
+// Repeat
+// Console.log the array
+}
