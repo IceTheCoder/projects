847271b42cee6d2c05579f95775cadcf001cdbb7
IceTheCoder <cristian.gabriel131408@gmail.com>
2024-09-19 07:25:32 +0300

    reading arrays

--- array-transformation/script.js
+++ array-transformation/script.js
@@ -1,15 +1,21 @@
-let array = [];
-
-// Define function to parse array
 function parseArray() {
-
-// Define an empty array
-  array = [];
-// place the text of the input into a variable (string)
+  let array = [];
   let text = document.getElementById("array").value;
-  console.log(text);
-// Read until a comma
-// push everything that has been read into the array
-// Repeat
-// Console.log the array
+  let element = "";
+  for (let i = 0; i < text.length; i++) {
+    if (Number.isNaN(text[i]) && text[i] !== ",") {
+      alert("Type a valid array!");
+      break;
+    }
+    if (text[i] === ",") {
+      array.push(Number(element));
+      element = "";
+      continue;
+    }
+    element += text[i];
+    if (i === text.length - 1) {
+      array.push(Number(element));
+    }
+  }
+  console.log(array);
 }
