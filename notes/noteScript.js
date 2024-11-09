ba91a5976173b027977f6d4738702bc0cbbbc6f7
IceTheCoder <cristian.gabriel131408@gmail.com>
2024-11-09 11:06:36 +0200

    saving notes

--- notes/noteScript.js
+++ notes/noteScript.js
@@ -5,7 +5,9 @@ document.addEventListener('keydown', function(event) {
   const title = document.getElementById("note-title").innerHTML;
   const content = document.getElementById("note-content").innerHTML;
 
-  localStorage.setItem(localStorage.getItem("loadedNote"), title);
+  const entry = { title, content };
+
+  localStorage.setItem(localStorage.getItem("loadedNote"), JSON.stringify(entry));
   console.log("Saving note...");
 });
 
